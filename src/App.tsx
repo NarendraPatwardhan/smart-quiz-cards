cfe4833377f532d9a92af7ef2ff37baaa960f9c8
Narendra Patwardhan <narendra@machinelearning.one>
2025-01-18 16:21:19 +0000

    Removed extra toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -7,7 +6,6 @@ import Index from "./pages/Index";
 const App = () => (
   <TooltipProvider>
     <Toaster />
-    <Sonner />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
